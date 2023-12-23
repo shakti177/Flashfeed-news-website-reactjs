2ef50aab9f1cb166a84970558d1247e03f62ba31
Shakti Tamrakar <shaktitamrakar14@gmail.com>
2023-12-23 17:55:36 +0530

    Added Header Logo and Route in the website

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Home from "./Pages/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
